8c92102240bc788eee5f497675f082107204d019
Vijay Barve <vijay.barve@gmail.com>
2017-07-21 02:31:08 +0000

    version 0.2.18

--- R/distrigraph.R
+++ R/distrigraph.R
@@ -1,6 +1,6 @@
 #' Distribution graphs
 #' 
-#' Build plots dispalying distribution of biodiversity records among 
+#' Build plots displaying distribution of biodiversity records among 
 #' user-defined features.
 #' 
 #' The main use of this function is to create record histograms according to
@@ -11,8 +11,9 @@
 #' @import sqldf
 #' @importFrom graphics hist plot
 #' @param indf input data frame containing biodiversity data set
-#' @param ptype Feature to represent. Accepted values are "species", "cell" and 
-#'   "efforts" (year)
+#' @param ptype Feature to represent. Accepted values are "species", "cell",  
+#'   "efforts" and "effortspecies" (year)
+#' @param cumulative with ptype as efforts, plot a cumulative records graph
 #' @param ... any additional parameters for the \code{\link{plot}} function.
 #' @examples \dontrun{
 #'  distrigraph(inat,ptype="cell",col="tomato")
@@ -21,7 +22,7 @@
 #'  distrigraph(inat,ptype="efforts",col="red",type="s")
 #' }
 #' @export
-distrigraph <- function(indf,ptype=NA,...){
+distrigraph <- function(indf,ptype=NA,cumulative=F,...){
   custgraph='col="red"'
   if(!is.na(ptype)){
     switch(ptype,
@@ -37,7 +38,30 @@ distrigraph <- function(indf,ptype=NA,...){
              Year_ = as.numeric(strftime(as.Date(indf$Date_collected,na.rm=T), format = "%Y"))
              indf=cbind(indf,Year_)
              mat=sqldf("select Year_, count(*) as Records from indf group by Year_ order by Year_")
-             plot(mat,main="Distribution of collection effforts over time",...)
+             if(!cumulative){
+               plot(mat,main="Distribution of collection effforts over time",...)
+             }else{
+               mat1 <- as.data.frame(cbind(mat$Year_, cumsum(mat$Records)))
+               names(mat1)<-c("Year","Records")
+               plot(mat1,main="Accumulation of collection effforts over time",...)
+             }
+           },
+           effortspecies={
+             Year_ = as.numeric(strftime(as.Date(indf$Date_collected,na.rm=T), format = "%Y"))
+             indf=cbind(indf,Year_)
+             matsp <- sqldf("select Scientific_name, Year_ from indf group by Scientific_name, Year_")
+             mat <- sqldf("select Year_, count(*) as Records from matsp group by Year_ order by Year_")
+             names(mat)<-c("Year","Species")
+             if(!cumulative){
+               plot(mat,main="Distribution of species collection effforts over time",...)
+             }else{
+               mat1 <- as.data.frame(cbind(mat$Year, cumsum(mat$Species)))
+               names(mat1)<-c("Year","Species")
+               for (i in 1:dim(mat1)[1]){
+                 mat1$Species[i] <- length(unique(matsp$Scientific_name[which(matsp$Year_<mat$Year[i])]))
+               }
+               plot(mat1,main="Accumulation of species over time",...)
+             }
            },
            stop("Not a valid option. See ?distrigraph for currently accepted values")
     )
